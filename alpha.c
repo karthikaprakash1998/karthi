e69445c279874063093fbbc710100a7c28ce7b9c
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-01 14:43:21 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int s;
+printf("enter the character");
+scanf("%c",&s);
+if(a||b||c||d||e|f||g||h||i||j||k||l||m|n||o||p||q||r||s||t||u||v||w||x||y||z)
+{
+print("the character is alphabet");
+}
+}
