5cd288bf37bb19364f695200065013f2d0471f8a
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-01 14:31:07 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+char c;
+printf("enter the string");
+scanf("%c",&c);
+if('a'||'e'||'i'||'o'||'u')
+{
+printf("the character is vowel");
+}
+else
+{
+printf("the character is constand");
+}
+}
