41412edf7fa8618f342d8c5207a6270786c560f5
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-02 19:00:11 +0530

    Create power.c

---
+++ power.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int b,e;
+
+    int result = 1;
+
+    printf("Enter a base number: ");
+    scanf("%d", &b);
+
+    printf("Enter an exponent: ");
+    scanf("%d", &e);
+
+    while (e != 0)
+    {
+        result=result* b;
+        --e;
+    }
+
+    printf("Answer = %lld", result);
+
+    return 0;
+}
+
