e62c0f17b06fb637120ec39f4ea1c78a3833962d
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-02 22:10:52 +0530

    Create sumodd.c

---
+++ sumodd.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int a,sum=0;
+printf("enter the numebr");
+scanf("%d",&a);
+for(i=0;i<15;i++)
+{
+sum=sum+i;
+}
+printf("%d",sum);
+for(a=15;a<45;a++)
+if(a%2==1)
+print("the num is odd");
+}
