3538d75de5a4c7f731bd62242a04daab1652ab18
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-02 21:39:21 +0530

    Create lcmprogram.c

---
+++ lcmprogram.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int n1, n2, minMultiple;
+    printf("Enter two positive integers: ");
+    scanf("%d %d", &n1, &n2);
+    lcm = (n1>n2) ? n1 : n2;
+    while(1)
+    {
+        if( lcm%n1==0 && lcm%n2==0 )
+        {
+            printf("The LCM of %d and %d is %d.", lcm);
+        
+        }
+        ++lcm;
+    }
+    return 0;
+}
