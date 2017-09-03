c801069794256ebdb92c1ca5646ab47ec5c2d42f
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-03 21:37:41 +0530

    Create out.c

---
+++ out.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,n;
+printf(enter the range");
+scanf("%d",&n);
+printf("enter the number");
+scanf("%d",&i);
+if(i>=n)
+{
+printf("out of order,error");
+}
+else
+{
+printf("pls enter valid input");
+}
+}
