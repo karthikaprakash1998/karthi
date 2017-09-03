a68ba2d8ddbd1e92ccd42ae6c5ccfc89c3492956
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-03 22:07:28 +0530

    Update bool.c

--- bool.c
+++ bool.c
@@ -1,24 +1,23 @@
-#include<stdio.h>
-#define bool int
-bool isPowerOfTwo(int n)
+#include <stdio.h> 
+int isPowerOf2(int number)
 {
-  if (n == 0)
-    return 0;
-  while (n != 1)
-  {
-    if (n%2 != 0)
-      return 0;
-    n = n/2;
-  }
-  return 1;
+    while(number!=1)
+    {
+        if(number%2!=0)
+            return 0;
+        number=number/2;
+    }
+    return 1;
 }
 int main()
 {
-  isPowerOfTwo(31)? printf("Yes\n"): printf("No\n");
-  isPowerOfTwo(17)? printf("Yes\n"): printf("No\n");
-  isPowerOfTwo(16)? printf("Yes\n"): printf("No\n");
-  isPowerOfTwo(2)? printf("Yes\n"): printf("No\n");
-  isPowerOfTwo(18)? printf("Yes\n"): printf("No\n");
-  isPowerOfTwo(1)? printf("Yes\n"): printf("No\n");
-  return 0;
+    int num;
+    printf("Enter an integer number: ");
+    scanf("%d",&num);
+    if(isPowerOf2(num))
+        printf("%d is a number that is the power of 2.",num);
+    else
+        printf("%d is not the power of 2.",num);
+     
+    return 0;
 }
