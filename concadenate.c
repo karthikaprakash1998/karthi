c109518f240988680958407e2684eb69bfcdc1ff
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2017-09-03 20:50:13 +0530

    Create concadenate.c

---
+++ concadenate.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+char str1[50],str2[50];
+int i,j;
+printf("enter the two string");
+gets(str1);
+gets(str2);
+for(i=0;str[i]='\0';i++)
+{
+for(j=0;str2[j]='\0';j++)
+{
+str1[i]=str2[j];
+i++;
+}
+printf("str1");
+}
+
+
