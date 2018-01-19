ae342c4716c73a0c6917abab02829ea9c920fb0f
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2018-01-19 11:30:44 +0000

    Create 56.c

---
+++ 56.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+char str;
+printf("enter the string");
+gtes(str);
+if((str>a)&&(str<z)||(str>0)&&(str<9))
+{
+printf("it contains alphabet and numbers");
+}
+else
+{
+printf("no numbers and alphabets");
+}}
