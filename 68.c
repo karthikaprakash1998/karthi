7ed669e31821068d16703afcb749b1a305c247d9
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2018-01-19 10:25:54 +0000

    Create 68.c

---
+++ 68.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("enter the number");
+scanf("%d",&n);
+if(n%7==0)
+{
+printf("the number is multiple of 7");
+}
+else
+{
+printf("the numebr is not mulyiple of 7");
+}
+}
