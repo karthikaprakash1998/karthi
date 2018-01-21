ec50824262453a9dbbb6e5e01755cbd3360e36d2
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2018-01-21 13:39:39 +0530

    Create 71.C

---
+++ 71.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int temp;
+char str[100];
+printf("enter the character");
+gets(str);
+temp=str;
+strrev(str);
+if(str==temp)
+{
+printf("the string is palindrome");
+}
+else
+{
+printf("not a palindrome");
+}
+}
