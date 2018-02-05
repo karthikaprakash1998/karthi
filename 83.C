51e26f34cfb4d4009f1ad7ef2bfc2b0a438d1408
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2018-02-05 16:06:34 +0530

    Update 83.C

--- 83.C
+++ 83.C
@@ -1,10 +1,10 @@
 #include<stdio.h>
 void main()
 {
-int i,j,mod,div;
+int i,j,mod,divi;
 printf("enter the values");
 scanf("%d%d",&i,&j);
 mod=i%j;
-div=i/j;
-printf("%d\n%d\n",mod,div);
+divi=i/j;
+printf("%d\n%d\n",mod,divi);
 }
