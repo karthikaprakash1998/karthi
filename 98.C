490e4158a20221fbfde18c240cf1c31814f4d739
karthikaprakash1998 <30829446+karthikaprakash1998@users.noreply.github.com>
2018-02-12 16:43:57 +0530

    Create 98.C

---
+++ 98.C
@@ -0,0 +1,26 @@
+#include<stdio.h>
+void main()
+{
+int n,a[10],i,j,temp,c=0;
+printf("enter the number");
+scanf("%d",&n);
+printf("enter the values in ascending oreder with one change");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<n;i++)
+{
+for(j=i+1;j<n;j++)
+{
+    if(a[i]>a[j])
+    {
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+c++;
+}
+}
+}
+printf("%d",c);
+}
